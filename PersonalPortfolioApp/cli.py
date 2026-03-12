f7800a0e3667be569aad5f92fcc04293d4a15dd6
Logan Berk <146039828+LoganOBerk@users.noreply.github.com>
2026-03-11 23:36:39 -0400

    reset user at logout

--- PersonalPortfolioApp/cli.py
+++ PersonalPortfolioApp/cli.py
@@ -13,6 +13,7 @@ class Cli:
 
         while True:
             selection = 0
+            self.userAccount = None
             # TODO: Welcome menu display
             # TODO: Display selection options
             # TODO: Selection input receiver
