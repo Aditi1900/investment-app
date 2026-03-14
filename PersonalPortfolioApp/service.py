83370105a06b801f913120a816456701c72cd901
Logan Berk <146039828+LoganOBerk@users.noreply.github.com>
2026-03-14 01:44:56 -0400

    stripped service to strict service actions, waiting till db is finished to itterate further

--- PersonalPortfolioApp/service.py
+++ PersonalPortfolioApp/service.py
@@ -50,27 +50,6 @@ class Service:
             #TODO: update the db
             pass
         pass
-
-    def populate_userAccount(self, userAccount : User) -> None:
-        self.populate_portfolios(userAccount, userAccount.portfolios)
-
-    def populate_portfolio(self, userAccount : User, portfolio : Portfolio) -> None:
-        #TODO: Pull portfolio data from db with context
-        #TODO: Populate portfolio with data
-        self.populate_stocks(userAccount, portfolio, portfolio.stocks)
-
-    def populate_stock(self, userAccount : User, portfolio : Portfolio, stock : Stock) -> None:
-        #TODO: Pull stock data from db with context
-        #TODO: Populate stock with data
-        pass
-
-    def populate_portfolios(self, userAccount: User, portfolios : dict[str, Portfolio]) -> None:
-        for portfolio in portfolios.values():
-            self.populate_portfolio(userAccount, portfolio)
-
-    def populate_stocks(self, userAccount : User, portfolio : Portfolio, stocks : dict[str, Stock]) -> None:
-        for stock in stocks.values():
-            self.populate_stock(userAccount, portfolio, stock)
         
 
     @staticmethod
