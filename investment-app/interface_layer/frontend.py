cf821f4b77ef5d4946da8b21d9df746ca08c312d
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2026-03-26 10:08:25 -0400

    added basic CORS middleware to prevent browser blocking

--- investment-app/interface_layer/frontend.py
+++ investment-app/interface_layer/frontend.py
@@ -1,12 +1,17 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from integration_layer import FrontendApi, router, init
 
 class Frontend:
     def __init__(self, service, validator):
+
+        origins = ["http://localhost:3000"]
+
         self.api = FrontendApi(service, validator)
         self.app = FastAPI()
+        self.app.add_middleware(CORSMiddleware, allow_origins = origins, allow_methods = ['*'], allow_headers = ['*'])
         init(self.api)
         self.app.include_router(router)
 
