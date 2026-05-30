b04b69d9da1276d10bffe93d72f2307acfecd949
Logan Berk <146039828+LoganOBerk@users.noreply.github.com>
2026-05-29 20:49:58 -0400

    tested livecache and ensured it included locks to avoid race conditions, moved volatility injecter into livecache instead of service and now when server volatility is present the viewed price and sale price match

--- src/edgexchange/integration_layer/livecache.py
+++ src/edgexchange/integration_layer/livecache.py
@@ -1,24 +1,31 @@
 import time
 import threading
 
+from threading import Lock
 from collections import defaultdict
 
+from ..common.entropy import inject_volatility
 from .externalapi import ExternalApi as eapi
 
 
 cache = defaultdict(lambda : {"price" : None, "float" : None, "timestamp" : None})
+cache_lock = Lock()
 
 def run():
     while True:
         expired = []
         now = time.time()
+        with cache_lock:
+            for ticker, data in cache.items():
+                timestamp = data["timestamp"]
+                if timestamp is not None and now - timestamp > 1: 
+                    expired.append(ticker)
+                else: 
+                    if data["price"] is not None:
+                        data["price"] += inject_volatility(data["price"])
 
-        for ticker, data in cache.items():
-            timestamp = data["timestamp"]
-            if timestamp is not None and now - timestamp > 1: expired.append(ticker)
-
-        for ticker in expired:
-            del cache[ticker]
+            for ticker in expired:
+                del cache[ticker]
 
         time.sleep(1)
 
@@ -30,18 +37,19 @@ class LiveCache:
     # INPUT/OUTPUT/PRECONDITION/POSTCONDITION/RAISES: see respective ExternalApi.get_stock_price() fields
     @staticmethod
     def get_stock_price(ticker : str) -> float:
-        if cache[ticker]["price"] is None:
-            cache[ticker]["price"] = eapi.get_stock_price(ticker)
-            cache[ticker]["timestamp"] = time.time()
+        with cache_lock:
+            if cache[ticker]["price"] is None:
+                cache[ticker]["price"] = eapi.get_stock_price(ticker)
+                cache[ticker]["timestamp"] = time.time()
 
-        price = cache[ticker]["price"]
+            price = cache[ticker]["price"]
 
         return price
 
 
     # INPUT/OUTPUT/PRECONDITION/POSTCONDITION/RAISES: see respective ExternalApi.get_stock_price() fields
     @staticmethod
-    def does_ticker_exist(self, ticker : str) -> bool:
+    def does_ticker_exist(ticker : str) -> bool:
         exist = True
 
         if ticker not in cache:
@@ -53,10 +61,12 @@ class LiveCache:
     # INPUT/OUTPUT/PRECONDITION/POSTCONDITION/RAISES: see respective ExternalApi.get_stock_price() fields
     @staticmethod
     def get_float(ticker : str) -> int:
-        if cache[ticker]["float"] is None:
-            cache[ticker]["float"] = eapi.get_float(ticker)
+        
+        with cache_lock:
+            if cache[ticker]["float"] is None:
+                cache[ticker]["float"] = eapi.get_float(ticker)
 
-        max_shares = cache[ticker]["float"]
+            max_shares = cache[ticker]["float"]
 
         return max_shares
 
@@ -66,17 +76,19 @@ class LiveCache:
     def get_stock_prices(tickers: list[str]) -> dict[str, float]:
         ticker_package = {}
 
-        cached_tickers = list(set(tickers) & set(cache))
-        missing_tickers = list(set(tickers) - set(cache))
+        with cache_lock:
+            cached_tickers = list(set(tickers) & set(cache))
+            missing_tickers = list(set(tickers) - set(cache))
 
         fresh = eapi.get_stock_prices(missing_tickers)
-        
-        for ticker, price in fresh.items():
-            cache[ticker]["price"] = price
-            cache[ticker]["timestamp"] = time.time()
 
-        for ticker in cached_tickers:
-            ticker_package[ticker] = cache[ticker]["price"]
+        with cache_lock:
+            for ticker, price in fresh.items():
+                cache[ticker]["price"] = price
+                cache[ticker]["timestamp"] = time.time()
+
+            for ticker in cached_tickers:
+                ticker_package[ticker] = cache[ticker]["price"]
 
         ticker_package |= fresh
 
