709bce9818fa2940ddaca60bbec854aa93771e0f
Logan Berk <146039828+LoganOBerk@users.noreply.github.com>
2026-06-07 19:35:49 -0400

    formatting changes made to externalapi

--- src/edgexchange/integration_layer/externalapi.py
+++ src/edgexchange/integration_layer/externalapi.py
@@ -2,13 +2,16 @@ import logging
 from math import inf
 
 import yfinance as yf
-from ..common.errors import FetchingError 
+from ..common.errors import FetchingError
+
 logging.getLogger("yfinance").setLevel(logging.CRITICAL)
+
 
 # PURPOSE:
 #   -ExternalApi provides a external finance fetching abstraction
 #   -provides functionality related to fetching live stock data
 class ExternalApi:
+
 
     # INPUT:
     #   -ticker(str); a stock ticker symbol
@@ -18,10 +21,11 @@ class ExternalApi:
     #   -ticker; exists in open market
     # POSTCONDITION:
     #   -price; current market price for ticker
-    # RAISES: 
+    # RAISES:
     #   -FetchingError; if yfinance call fails
     @staticmethod
-    def get_stock_price(ticker : str) -> float:
+    def get_stock_price(ticker: str) -> float:
+
         try:
 
             price = yf.Ticker(ticker).fast_info.last_price
@@ -33,17 +37,18 @@ class ExternalApi:
 
 
     # INPUT:
-    #   -ticker(str); a stock ticker symbol 
+    #   -ticker(str); a stock ticker symbol
     # OUTPUT:
     #   -exist(bool); whether ticker exists in the open market
     # PRECONDITION:
     #   -ticker; matches format [A-Z]{1,5}
     # POSTCONDITION:
     #   -exist; True if ticker exists in open market, False otherwise
-    # RAISES: 
+    # RAISES:
     #   -FetchingError; if yfinance call fails
     @staticmethod
-    def does_ticker_exist(ticker : str) -> bool:
+    def does_ticker_exist(ticker: str) -> bool:
+
         try:
 
             exist = yf.Ticker(ticker).fast_info.last_price is not None
@@ -55,25 +60,28 @@ class ExternalApi:
 
 
     # INPUT:
-    #   -ticker(str); a stock ticker symbol 
+    #   -ticker(str); a stock ticker symbol
     # OUTPUT:
     #   -max_shares(float); total shares available in open market
     # PRECONDITION:
     #   -ticker; exists in open market
     # POSTCONDITION:
     #   -max_shares; total float shares available in open market for ticker, otherwise inf
-    # RAISES: 
+    # RAISES:
     #   -FetchingError; if yfinance call fails
     @staticmethod
-    def get_float(ticker : str) -> int:
+    def get_float(ticker: str) -> int:
+
         try:
 
-            max_shares = yf.Ticker(ticker).fast_info.get('floatShares')
+            raw = yf.Ticker(ticker).fast_info.get('floatShares')
 
         except Exception as e:
             raise FetchingError(f"get_float failed: {e}") from e
 
-        return max_shares if max_shares is not None else inf
+        max_shares = raw if raw is not None else inf
+
+        return max_shares
 
 
     # INPUT:
@@ -84,17 +92,19 @@ class ExternalApi:
     #   -tickers; exist in open market
     # POSTCONDITION:
     #   -ticker_package; holds current market prices for tickers and the ticker symbol related
-    # RAISES: 
+    # RAISES:
     #   -FetchingError; if yfinance call fails at any point
     @staticmethod
     def get_stock_prices(tickers: list[str]) -> dict[str, float]:
+
         ticker_package = {}
 
         try:
 
             ticker_dat = None if not tickers else yf.Tickers(" ".join(tickers))
-        
+
             for t in tickers:
+
                 price = ticker_dat.tickers[t].fast_info.last_price
 
                 if not price:
@@ -104,6 +114,7 @@ class ExternalApi:
                     raise Exception(t)
 
                 ticker_package[t] = price
+
 
         except Exception as e:
             raise FetchingError(f"get_stock_prices failed: {e}") from e
