b9de41163ba36957578287872c607421c3ab2154
Logan Berk <146039828+LoganOBerk@users.noreply.github.com>
2026-06-22 11:05:59 -0400

    Update livecache.py
    On further evaluation previous edge case was already handled

--- src/edgexchange/integration_layer/livecache.py
+++ src/edgexchange/integration_layer/livecache.py
@@ -130,9 +130,7 @@ def run():
             with cache_lock:
                 for ticker, price in ticker_prices.items():
                     price += inject_volatility(price)
-                    signal.wait_for(lambda: read(ticker, "quote") is not None or ticker not in cache)
-                    if ticker not in cache: continue
-                
+                    signal.wait_for(lambda: read(ticker, "quote") is not None)
                     write([ticker, "quote", "price"], price)
                     write([ticker, "price"], price)
                     write([ticker, "timestamp"], time.time())
